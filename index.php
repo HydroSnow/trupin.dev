ce36e3219d42fac64508716114b4ea8657d91d4b
Alexis Trupin <link9745@gmail.com>
2019-10-19 15:21:33 +0200

    redoing site

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 		<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="/lib/bootstrap-4.3.1-dist/css/bootstrap.min.css" />
 		<style>
-			html, body {
-				height: 100%;
-				width: 100%;
+			body {
+				text-align: center;
 			}
 
-			body {
+			header {
+				min-height: 100vh;
+
 				background-image: linear-gradient(
 					rgba(0, 0, 0, 0.6),
 					rgba(0, 0, 0, 0.6)
@@ -35,7 +36,7 @@
 	            justify-content: center;
 			}
 
-			#particles-js {
+			header #particles {
 				position: absolute;
 				width: 100%;
 				height: 100%;
@@ -43,31 +44,154 @@
 				z-index: 0;
 			}
 
-			#content {
+			header #boxes {
 				z-index: 1;
-
-				color: white;
-				text-align: center;
 			}
+
+			section {
+				padding-top: 3em;
+				padding-bottoM: 3em;
+			}
+
+			.link {
+				margin-top: 2em;
+				margin-bottom: 2em;
+			}
+
+			.link img {
+				width: 30%;
+				margin-bottom: 1em;
+			}
+
+			.portfolio {
+				display: flex;
+				padding: 1em;
+				margin-top: 1em;
+				margin-bottom: 1em;
+			}
+
+			.portfolio > img {
+				height: 6em;
+			}
+
+			.portfolio > div {
+				text-align: left;
+				padding-left: 1em;
+			}
+
+			footer {
+				padding-top: 2em;
+				padding-bottom: 1em;
+			}
+
+			.info {
+				margin-top: 1em;
+				margin-bottom: 1em;
+			}
+
 		</style>
 	</head>
 	<body>
-		<div id="particles-js"></div>
-		<div id="content" class="p-3 bg-dark border rounded">
-			<h1 class="mx-3 mt-3 mb-1">Alexis Trupin</h1>
-			<p class="mx-3 mt-1 mb-3 lead">Développeur</p>
-			<p class="m-3">
-				<a href="https://github.com/HydroSnow"><button type="button" class="btn btn-primary">GitHub</button></a>
-				<a href="/portfolio.php"><button type="button" class="btn btn-primary">Portfolio</button></a>
-			</p>
-			<p class="m-3">trupin.alexis@gmail.com</p>
-		</div>
+		<header>
+			<div id="particles"></div>
+			<div id="boxes" class="text-white">
+				<h1 style="font-size:5rem;">Alexis Trupin</h1>
+				<p class="lead" style="font-size:3rem;">Développeur</p>
+				<a href="#1">
+					<img style="width:3em;" src="/img/arrow.png" />
+				</a>
+			</div>
+		</header>
+		<section id="1" class="bg-primary">
+			<div class="container text-white">
+				<div class="row">
+					<div class="col-sm-6 col-md-4 link">
+						<img src="/img/link/github.png" />
+						<h3>GitHub</h3>
+						<a href="https://github.com/HydroSnow" class="text-white">github.com/HydroSnow</a>
+					</div>
+					<div class="col-sm-6 col-md-4 link">
+						<img src="/img/link/linkedin.png" />
+						<h3>LinkedIn</h3>
+						<a href="https://linkedin.com/in/trupin/" class="text-white">linkedin.com/in/trupin/</a>
+					</div>
+					<div class="col-sm-6 col-md-4 link">
+						<img src="/img/link/mail.png" />
+						<h3>E-Mail</h3>
+						<a href="mailto:trupin.alexis@gmail.com" class="text-white">trupin.alexis@gmail.com</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="2" class="bg-white">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<h1>Portfolio</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-12 col-md-6 portfolio p-3">
+						<img class="img-fluid" src="/img/portfolio/discord.png" alt="Logo Discord">
+						<div>
+							<h2><a href="#" class="text-dark">Bots Discord</a></h2>
+							<p class="mb-0">todo: description</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 portfolio p-3">
+						<img class="img-fluid" src="/img/portfolio/fromage.jpg" alt="Fromage">
+						<div>
+							<h2><a href="#" class="text-dark">Les Fromageries Wankul</a></h2>
+							<p class="mb-0">todo: description</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 portfolio p-3">
+						<img class="img-fluid" src="/img/portfolio/aedificantes.jpg" alt="Logo Aedificantes">
+						<div>
+							<h2><a href="#" class="text-dark">Stage chez Aedificantes</a></h2>
+							<p class="mb-0">todo: description</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 portfolio p-3">
+						<img class="img-fluid" src="/img/portfolio/24h.jpg" alt="Logo 24h du Code">
+						<div>
+							<h2><a href="#" class="text-dark">Les 24h du Code</a></h2>
+							<p class="mb-0">todo: description</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="bg-dark text-white">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4 info">
+						<h4>Crédits</h4>
+						<p>
+							<a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>,
+							<a href="https://jquery.com/" class="text-white">jQuery</a>,
+							<a href="https://popper.js.org/" class="text-white">popper.js</a>,
+							<a href="https://vincentgarreau.com/particles.js/" class="text-white">particles.js</a>
+						</p>
+					</div>
+					<div class="col-sm-4 info">
+						<h4>Copyright</h4>
+						<?php $b = "2019"; $n = date("Y"); $copy_year = ($n == $b ? $b : $b . " - " . $n); ?>
+						<p><a href="https://github.com/HydroSnow/trupin.dev/blob/master/LICENSE" class="text-white"><?= $copy_year ?> &copy; Alexis Trupin</a></p>
+					</div>
+					<div class="col-sm-4 info">
+						<h4>Source</h4>
+						<p><a href="https://github.com/HydroSnow/trupin.dev" class="text-white">github.com/trupin.dev</a></p>
+					</div>
+				</div>
+			</div>
+		</footer>
 		<script src="/lib/jquery-3.4.1.min.js"></script>
 		<script src="/lib/popper-1.15.0.min.js"></script>
 		<script src="/lib/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
 		<script src="/lib/particles.js-2.0.0/particles.min.js"></script>
 		<script>
-			particlesJS.load('particles-js', '/lib/particles.js-2.0.0/particles.json', function() {
+			particlesJS.load('particles', '/lib/particles.js-2.0.0/particles.json', function() {
 				console.log('callback - particles.js config loaded');
 			});
 		</script>
