f0139dcc1d4017a897acb56beb65616d626b6478
Alexis Trupin <link9745@gmail.com>
2019-11-10 00:02:01 +0100

    moving favicon into assets folder

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<meta name="description" content="<?= $description ?>" />
-	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
 	<link rel="stylesheet" type="text/css" href="/vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css" />
 	<style>
 		html {
