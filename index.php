a786041ff0dc9e68892359765c9dda3020c1a5c3
Alexis Trupin <link9745@gmail.com>
2019-11-21 21:12:29 +0100

    putting style in separate files

--- index.php
+++ index.php
@@ -37,175 +37,14 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<meta name="description" content="<?= $description ?>" />
+	<meta name="theme-color" content="#010222" />
 	<link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
 	<link rel="stylesheet" type="text/css" href="/node_modules/bootstrap/dist/css/bootstrap.min.css" />
-	<style>
-		html {
-			scroll-behavior: smooth;
-		}
-
-		body {
-			text-align: center;
-			font-size: 1.2em;
-		}
-
-		#particles-switch {
-		    position: absolute;
-		    z-index: 1;
-
-			left: 10px;
-			top: 10px;
-		}
-
-		#particles-switch>a {
-			color: gray;
-		}
-
-		header {
-			min-height: 100vh;
-
-			display: -ms-flexbox;
-			display: -webkit-box;
-			display: flex;
-
-			flex-direction: column;
-
-			-ms-flex-align: center;
-			-ms-flex-pack: center;
-			-webkit-box-align: center;
-			align-items: center;
-			-webkit-box-pack: center;
-			justify-content: center;
-
-			background-image: linear-gradient(#00000055, #00000055), url('/assets/snow-village.jpg');
-			background-position: center top;
-			-webkit-background-size: cover;
-			background-size: cover;
-
-			color: white;
-		}
-
-		header>.titles {
-		    z-index: 1;
-
-		    animation: fadein 2s;
-		    -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
-		    -moz-animation: fadein 2s; /* Firefox < 16 */
-		    -ms-animation: fadein 2s; /* Internet Explorer */
-		    -o-animation: fadein 2s; /* Opera < 12.1 */
-		}
-
-		<?php if ($particles) { ?> header>#particles {
-		    position: absolute;
-		    z-index: 0;
-
-		    width: 100%;
-		    height: 100%;
-
-		    animation: fadein 10s;
-		    -webkit-animation: fadein 10s; /* Safari, Chrome and Opera > 12.1 */
-		    -moz-animation: fadein 10s; /* Firefox < 16 */
-		    -ms-animation: fadein 10s; /* Internet Explorer */
-		    -o-animation: fadein 10s; /* Opera < 12.1 */
-		} <?php } ?>
-
-		@keyframes fadein {
-		    from { opacity: 0; } to { opacity: 1; }
-		}
-
-		@-moz-keyframes fadein { /* Firefox < 16 */
-		    from { opacity: 0; } to { opacity: 1; }
-		}
-
-		@-webkit-keyframes fadein { /* Safari, Chrome and Opera > 12.1 */
-		    from { opacity: 0; } to { opacity: 1; }
-		}
-
-		@-ms-keyframes fadein { /* Internet Explorer */
-		    from { opacity: 0; } to { opacity: 1; }
-		}
-
-		@-o-keyframes fadein { /* Opera < 12.1 */
-		    from { opacity: 0; } to { opacity: 1; }
-		}
-
-		.content-line {
-			display: inline-block;
-
-			height: 1px;
-		}
-
-		section {
-			padding-top: 3em;
-			padding-bottoM: 3em;
-		}
-
-		.nice-blue {
-			background-color: #a5d4f0;
-		}
-
-		a {
-			color: navy;
-		}
-
-		a:hover {
-			color: blue;
-		}
-
-		.bg-dark a {
-			color: white;
-		}
-
-		.bg-dark a:hover {
-			color: #ccf;
-		}
-
-		.portfolio {
-			display: flex;
-			padding: 1em;
-			margin-top: 1em;
-			margin-bottom: 1em;
-		}
-
-		.portfolio>img {
-			height: 6em;
-		}
-
-		.portfolio>div {
-			text-align: left;
-			padding-left: 1em;
-		}
-
-		.link {
-			margin-top: 2em;
-			margin-bottom: 2em;
-		}
-
-		.link>img {
-			width: 100px;
-			margin-bottom: 1em;
-		}
-
-		footer {
-			padding-top: 2em;
-			padding-bottom: 2em;
-			color: white;
-		}
-
-		.info {
-			margin-top: 1em;
-			margin-bottom: 1em;
-		}
-
-		footer h4 {
-			margin-bottom: 0.6em;
-		}
-
-		p {
-			margin: 0;
-		}
-	</style>
-	<meta name="theme-color" content="#010222" />
+	<link rel="stylesheet" type="text/css" href="/assets/css/header.css" />
+	<link rel="stylesheet" type="text/css" href="/assets/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="/assets/css/portfolio.css" />
+	<link rel="stylesheet" type="text/css" href="/assets/css/contact.css" />
+	<link rel="stylesheet" type="text/css" href="/assets/css/footer.css" />
 </head>
 
 <body>
@@ -232,13 +71,13 @@
 	</p>
 	<header class="py-5">
 		<?php if ($particles) { ?> <div id="particles"></div> <?php } ?>
-		<h1 class="boxes titles" style="font-size:5rem;">Alexis Trupin</h1>
-		<p class="lead mb-3 titles" style="font-size:3rem;">Développeur</p>
-		<a class="titles" href="#1">
+		<h1 class="header-content" style="font-size:5rem;">Alexis Trupin</h1>
+		<p class="lead mb-3 header-content" style="font-size:3rem;">Développeur</p>
+		<a class="header-content" href="#1">
 		    <img style="width:50px;" src="/assets/arrow.png" />
 		</a>
 	</header>
-	<section id="1" class="nice-blue">
+	<section id="1" class="section-blue">
 		<div class="container">
 			<h1 class="display-4">Présentation</h1>
 			<div class="content-line" style="background-color:black; width:100px;"></div>
@@ -248,7 +87,7 @@
 			</p>
 		</div>
 	</section>
-	<section class="bg-white">
+	<section class="section-white">
 		<div class="container">
 			<h1 class="display-4">Portfolio</h1>
 			<div class="content-line" style="background-color:black; width:100px;"></div>
@@ -289,22 +128,22 @@
 			<p>Le portfolio est actuellement incomplet. Vous pouvez consulter quelques unes de mes autres réalisations sur <a href="https://github.com/HydroSnow">github.com/HydroSnow</a>.</p>
 		</div>
 	</section>
-	<section class="nice-blue">
+	<section class="section-blue">
 		<div class="container">
 			<h1 class="display-4">Contact</h1>
 			<div class="content-line" style="background-color:black; width:100px;"></div>
 			<div class="row">
-				<div class="col-md-4 link">
+				<div class="col-md-4 contact">
 					<img src="/assets/link/github.svg" />
 					<h3>GitHub</h3>
 					<a href="https://github.com/HydroSnow">github.com/HydroSnow</a>
 				</div>
-				<div class="col-md-4 link">
+				<div class="col-md-4 contact">
 					<img src="/assets/link/linkedin.svg" />
 					<h3>LinkedIn</h3>
 					<a href="https://linkedin.com/in/trupin/">linkedin.com/in/trupin</a>
 				</div>
-				<div class="col-md-4 link">
+				<div class="col-md-4 contact">
 					<img src="/assets/link/mail.svg" />
 					<h3>E-Mail</h3>
 					<a href="mailto:trupin.alexis@gmail.com">trupin.alexis@gmail.com</a>
