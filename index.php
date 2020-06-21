621b881fe06ae8524bad4616fe581e144cae87d7
Alexis Trupin <link9745@gmail.com>
2020-06-22 00:14:48 +0200

    adding google analytics

--- index.php
+++ index.php
@@ -37,6 +37,14 @@
 <html lang="fr-FR">
 
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-170081437-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-170081437-1');
+	</script>
 	<title>Alexis Trupin</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
