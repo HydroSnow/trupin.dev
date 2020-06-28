6eb7010998dc04429c5859dceca5d393028c8553
Alexis Trupin <link9745@gmail.com>
2020-06-28 15:45:18 +0200

    using npm for dependancies

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
 		}
 	</script>
 	<?php if ($particles) { ?>
-		<script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.js" integrity="sha256-icjghcPaibMf1jv4gQIGi5MeWNHem2SispcorCiCfSg=" crossorigin="anonymous"></script>
+		<script src="/node_modules/particles.js/particles.js"></script>
 		<script async>
 			particlesJS.load('particles', '/assets/particles.json?v=<?= $ASSETS_VER ?>', async function () {
 				console.log('callback - particles.js config loaded');
